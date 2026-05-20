8ee686aeb3e1bef4d1f938108d64cde5d9e26370
Valerie Bolden <valerie.bolden@gmail.com>
2026-05-20 17:53:36 -0400

    required bcrypt and jsonwebtoken; imported jwt secret from env file; created a post route for login endpoint

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,8 @@
 const userRouter = require('express').Router();
 const User = require('../models/User');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const secret = process.env.JWT_SECRET;
 
 userRouter.post("/register", async (req, res) => {
     try {
@@ -10,7 +13,7 @@ userRouter.post("/register", async (req, res) => {
 
         if (existingUser) {
             return res.status(400).json({
-                error: "A user with this email already exists"
+                error: "A user with this email already exists."
             });
         }
 
@@ -21,6 +24,49 @@ userRouter.post("/register", async (req, res) => {
         res.status(201).json({
             username: newUser.username,
             email: newUser.email
+        });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+userRouter.post("/login", async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        const user = await User.findOne({ email });
+
+        // CHECK IF USER IS ALREADY IN DATABASE
+        if (!user) {
+            return res.status(400).json({
+                error: "Incorrect email or password."
+            });
+        }
+
+        const correctPassword = await user.isCorrectPassword(password);
+
+        if (!correctPassword) {
+            return res.status(400).json({
+                error: "Incorrect email or password."
+            });
+        }
+
+        // PAYLOAD
+        const payload = {
+            _id: user.id,
+            username: user.username,
+        };
+
+        // CREATE TOKEN
+        const token = jwt.sign(payload, secret, { expiresIn: "1h" });
+
+        res.json({
+            token,
+            user: {
+                _id: user._id,
+                username: user.username,
+                email: user.email
+            }
         });
     } catch (error) {
         res.status(500).json({ error: error.message });
