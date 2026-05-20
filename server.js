d277dedd8a419b7944ba6316396d12fd2c96c164
Valerie Bolden <valerie.bolden@gmail.com>
2026-05-20 16:06:56 -0400

    added listen function for port and added a test route to check connection

--- server.js
+++ server.js
@@ -3,3 +3,17 @@ const app = express();
 require('dotenv').config();
 const PORT = process.env.PORT;
 const secret = process.env.JWT_SECRET;
+
+// DATABASE CONNECTION
+
+// MIDDLEWARE
+
+// ROUTES
+// app.get("/", (req, res) => {
+//     res.send("TEST ROUTE");
+// });
+
+// PORT
+app.listen(PORT, (req, res) => {
+    console.log(`Server is running on PORT: ${PORT}`);
+});
