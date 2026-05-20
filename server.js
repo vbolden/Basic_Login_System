c466793b7e2c674d13e3ba6ee3a383715dcd57a0
Valerie Bolden <valerie.bolden@gmail.com>
2026-05-20 16:17:55 -0400

    established connection to MongoDB database

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const app = express();
 require('dotenv').config();
+const mongoose = require('mongoose');
 const PORT = process.env.PORT;
 const secret = process.env.JWT_SECRET;
 
 // DATABASE CONNECTION
+mongoose.connect(process.env.MONGODB_URI);
+
+const connectdb = mongoose.connection;
+connectdb.on('error', (err) => console.log(err.message + ' is mongo not running?'));
+connectdb.on('connected', () => console.log('mongo connected'));
+connectdb.on('disconnected', () => console.log('mongo disconnected'));
+
 
 // MIDDLEWARE
 
